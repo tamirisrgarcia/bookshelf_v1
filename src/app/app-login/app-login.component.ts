81467422b56aed08699a2ec021628d0d76973eed
Isabela Ribeiro <isabela.ribeiro1211@gmail.com>
2022-01-29 13:23:46 -0300

    Rotina de limpar campos - Login e Cadastro

--- src/app/app-login/app-login.component.ts
+++ src/app/app-login/app-login.component.ts
@@ -1,54 +1,69 @@
 import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngneat/hot-toast';
-
 import { AutenticacaoFirebaseService } from './../servicosInterface/autenticacao-firebase.service';
 
 @Component({
   selector: 'app-app-login',
   templateUrl: './app-login.component.html',
-  styleUrls: ['./app-login.component.scss']
+  styleUrls: ['./app-login.component.scss'],
 })
 export class AppLoginComponent {
   formularioLogin = this.loginBuilder.group({
-    email: new FormControl('',[Validators.required, Validators.email]),
-    senha: new FormControl('', Validators.required)
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', Validators.required),
   });
 
-  hasUnitNumber=false;
+  hasUnitNumber = false;
 
   constructor(
     private loginBuilder: FormBuilder,
-    @Inject(MAT_DIALOG_DATA) public conteudo:string,
+    @Inject(MAT_DIALOG_DATA) public conteudo: string,
     private toast: HotToastService,
     private rotas: Router,
     private autenticacaoFirebaseService: AutenticacaoFirebaseService
-    ) {}
+  ) {}
 
-    get email(){
-      return this.formularioLogin.get('email')
-    }
+  get email() {
+    return this.formularioLogin.get('email');
+  }
 
-    get senha(){
-      return this.formularioLogin.get('senha')
+  get senha() {
+    return this.formularioLogin.get('senha');
+  }
+  loginFirebase() {
+    if (!this.formularioLogin.valid) {
+      return;
     }
-    loginFirebase(){
-      if(!this.formularioLogin.valid){
-        return;
-      }
-      const {email, senha} = this.formularioLogin.value;
-      this.autenticacaoFirebaseService.loginUsuario(email, senha)
+    const { email, senha } = this.formularioLogin.value;
+    this.autenticacaoFirebaseService
+      .loginUsuario(email, senha)
       .pipe(
         // Correção de Português no Toast
         this.toast.observe({
           success: 'Login válido, obrigado!',
           loading: 'Redirecionando...',
-          error: 'Algo deu errado, confira suas informações!'
+          error: 'Algo deu errado, confira suas informações!',
         })
-      ).subscribe(()=>{
-        this.rotas.navigate(['/cdd'])
-      })
+      )
+      .subscribe(() => {
+        this.rotas.navigate(['/cdd']);
+        this.limparLogin();
+      });
+  }
+  // Rotina de limpar campos de login
+  limparLogin() {
+    this.formularioLogin = new FormGroup({
+      email: new FormControl(''),
+      senha: new FormControl(''),
+    });
+    this.formularioLogin.reset();
   }
 }
